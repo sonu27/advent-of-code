ab69354d13a38386b5e9fbee8982e77fb47f336a
Amarjeet Singh Rai <git@arai.dev>
2023-12-09 17:55:54 +0000

    cleanup 2023 day 8

--- 2023/src/bin/08.rs
+++ 2023/src/bin/08.rs
@@ -1,7 +1,6 @@
-use std::collections::HashMap;
 use num_integer::lcm;
-
 use regex::Regex;
+use std::collections::HashMap;
 
 advent_of_code::solution!(8);
 
@@ -13,14 +12,18 @@ pub fn part_one(input: &str) -> Option<u32> {
 
     let mut m = HashMap::new();
     let re = Regex::new(r"(\w+)").unwrap();
-    let a: Vec<_> = lines.map(|line| {
-        let aa: Vec<_> = re.captures_iter(line).map(|cap| cap[0].to_string()).collect();
-        aa
-    }).collect();
-
-    for x in a {
-        m.insert(x.get(0).unwrap().clone(), (x.get(1).unwrap().clone(), x.get(2).unwrap().clone()));
-    }
+    lines
+        .map(|line| {
+            re.captures_iter(line)
+                .map(|cap| cap[0].to_string())
+                .collect::<Vec<_>>()
+        })
+        .for_each(|v| {
+            m.insert(
+                v.get(0).unwrap().clone(),
+                (v.get(1).unwrap().clone(), v.get(2).unwrap().clone()),
+            );
+        });
 
     let mut steps = 0;
     let mut curr = "AAA";
@@ -43,35 +46,35 @@ pub fn part_one(input: &str) -> Option<u32> {
     }
 }
 
-pub fn part_two(input: &str) -> Option<u64> {// use u64 because of overflow
+pub fn part_two(input: &str) -> Option<u64> {
     let mut lines = input.lines();
     let cmds = lines.next().unwrap().chars();
 
     lines.next(); // skip the blank line
 
     let mut m = HashMap::new();
-    let re = Regex::new(r"(\w+)").unwrap();
-    let a: Vec<_> = lines.map(|line| {
-        let aa: Vec<_> = re.captures_iter(line).map(|cap| cap[0].to_string()).collect();
-        aa
-    }).collect();
-
     let mut currs = vec![];
-    for x in a {
-        let key = x.get(0).unwrap().clone();
+    let re = Regex::new(r"(\w+)").unwrap();
+    lines
+        .map(|line| {
+            re.captures_iter(line)
+                .map(|cap| cap[0].to_string())
+                .collect::<Vec<_>>()
+        })
+        .for_each(|v| {
+            let key = v.get(0).unwrap().clone();
 
-        if key.ends_with('A') {
-            currs.push(key.clone());
-        }
+            if key.ends_with('A') {
+                currs.push(key.clone());
+            }
 
-        m.insert(key, (x.get(1).unwrap().clone(), x.get(2).unwrap().clone()));
-    }
+            m.insert(key, (v.get(1).unwrap().clone(), v.get(2).unwrap().clone()));
+        });
 
     let mut steps: u64 = 0;
     let mut step_counts = vec![0; currs.len()];
     loop {
         for c in cmds.clone() {
-            // println!("{:?}, {:?}", steps, currs);
             for i in 0..currs.clone().len() {
                 if currs[i].ends_with('Z') && step_counts[i] > 0 {
                     continue;
@@ -94,9 +97,9 @@ pub fn part_two(input: &str) -> Option<u64> {// use u64 because of overflow
             steps += 1;
 
             if currs.iter().all(|x| x.ends_with('Z')) {
-                let mut lcm_value = step_counts[0].clone();
+                let mut lcm_value = step_counts[0];
                 for count in &step_counts[1..] {
-                    lcm_value = lcm(lcm_value, count.clone());
+                    lcm_value = lcm(lcm_value, *count);
                 }
                 return Some(lcm_value);
             }
@@ -108,15 +111,19 @@ pub fn part_two(input: &str) -> Option<u64> {// use u64 because of overflow
 mod tests {
     use super::*;
 
-// #[test]
-    // fn test_part_one() {
-    //     let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-    //     assert_eq!(result, Some(2));
-    // }
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
+        assert_eq!(result, Some(2));
+    }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(6));
     }
 }
