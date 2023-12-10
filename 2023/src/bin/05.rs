73d33b68549439cb0e7545f8dbca911fafee8985
Amarjeet Singh Rai <git@arai.dev>
2023-12-10 19:40:59 +0000

    cleanup

--- 2023/src/bin/05.rs
+++ 2023/src/bin/05.rs
@@ -6,16 +6,22 @@ pub fn part_one(input: &str) -> Option<u32> {
     let a = input
         .trim_start_matches("seeds: ")
         .split(" map:")
-        .map(|s| s.lines()
-            .filter(|s| {
-                if let Some(c) = s.chars().next() {
-                    c.is_numeric()
-                } else {
-                    false
-                }
-            })
-            .map(|s| s.split(' ').map(|s| s.parse::<i64>().unwrap()).collect::<Vec<_>>())
-            .collect::<Vec<_>>())
+        .map(|s| {
+            s.lines()
+                .filter(|s| {
+                    if let Some(c) = s.chars().next() {
+                        c.is_numeric()
+                    } else {
+                        false
+                    }
+                })
+                .map(|s| {
+                    s.split(' ')
+                        .map(|s| s.parse::<i64>().unwrap())
+                        .collect::<Vec<_>>()
+                })
+                .collect::<Vec<_>>()
+        })
         .collect::<Vec<_>>();
 
     let mut seeds = a[0][0].clone();
@@ -27,7 +33,16 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let orig_seeds: Vec<i64> = input.lines().next().unwrap().split(": ").nth(1).unwrap().split_whitespace().map(|s| s.parse().unwrap()).collect();
+    let orig_seeds: Vec<i64> = input
+        .lines()
+        .next()
+        .unwrap()
+        .split(": ")
+        .nth(1)
+        .unwrap()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
     let mut seeds: Vec<i64> = orig_seeds.clone();
     for i in (0..seeds.len()).step_by(2) {
         seeds[i + 1] = seeds[i] + seeds[i + 1] - 1;
@@ -37,21 +52,22 @@ pub fn part_two(input: &str) -> Option<u32> {
     let a = input
         .trim_start_matches("seeds: ")
         .split(" map:")
-        .map(|s| s
-            .lines()
-            .filter_map(|s| {
-                if let Some(c) = s.chars().next() {
-                    if c.is_numeric() {
-                        let x = s.split(' ').map(|s| s.parse::<i64>().unwrap()).collect::<Vec<_>>();
-                        Some((x[0], x[1], x[2]))
-                    } else {
-                        None
+        .map(|s| {
+            s.lines()
+                .filter_map(|s| {
+                    if let Some(c) = s.chars().next() {
+                        if c.is_numeric() {
+                            let x = s
+                                .split(' ')
+                                .map(|s| s.parse::<i64>().unwrap())
+                                .collect::<Vec<_>>();
+                            return Some((x[0], x[1], x[2]));
+                        }
                     }
-                } else {
                     None
-                }
-            })
-            .collect::<Vec<_>>())
+                })
+                .collect::<Vec<_>>()
+        })
         .collect::<Vec<_>>();
 
     let mut seed_endpts = vec![0, i64::MAX];
@@ -60,14 +76,19 @@ pub fn part_two(input: &str) -> Option<u32> {
     }
 
     fn in_seed_range(seeds: &Vec<i64>, s: i64) -> bool {
-        (0..seeds.len()).step_by(2).any(|i| seeds[i] <= s && s <= seeds[i + 1])
+        (0..seeds.len())
+            .step_by(2)
+            .any(|i| seeds[i] <= s && s <= seeds[i + 1])
     }
 
-    let seed_endpts: Vec<i64> = seed_endpts.into_iter().filter(|&s| in_seed_range(&seeds, s)).collect();
+    let seed_endpts: Vec<i64> = seed_endpts
+        .into_iter()
+        .filter(|&s| in_seed_range(&seeds, s))
+        .collect();
 
     let mut ans = seed_endpts.clone();
-    for i in 1..a.len() {
-        ans = get_ans2(ans, &a[i]);
+    for i in a {
+        ans = get_ans2(ans, &i);
     }
     ans.iter().min().map(|s| *s as u32)
 }
@@ -81,26 +102,45 @@ fn inverse_lookup_map(map_info: &Vec<(i64, i64, i64)>, y: i64) -> i64 {
     y
 }
 
-fn invert_map(map_info: &Vec<(i64, i64, i64)>, output_endpts: &Vec<i64>) -> Vec<i64> {
-    let mut map_endpts: Vec<(i64, i64)> = map_info.iter().flat_map(|&(d_start, s_start, rlen)| vec![(d_start, s_start), (d_start + rlen - 1, s_start + rlen - 1)]).collect();
+fn invert_map(map_info: &Vec<(i64, i64, i64)>, output_endpts: &[i64]) -> Vec<i64> {
+    let mut map_endpts: Vec<(i64, i64)> = map_info
+        .iter()
+        .flat_map(|&(d_start, s_start, rlen)| {
+            vec![(d_start, s_start), (d_start + rlen - 1, s_start + rlen - 1)]
+        })
+        .collect();
     map_endpts.sort_by(|a, b| a.0.cmp(&b.0));
 
-    let mut output_src_endpts: Vec<i64> = output_endpts.iter().map(|&y| inverse_lookup_map(map_info, y)).collect();
+    let mut output_src_endpts: Vec<i64> = output_endpts
+        .iter()
+        .map(|&y| inverse_lookup_map(map_info, y))
+        .collect();
     output_src_endpts.sort();
 
-    let mut input_src_endpts: Vec<i64> = map_endpts.iter().map(|&(_y, x)| x).collect::<HashSet<_>>().into_iter().collect();
+    let mut input_src_endpts: Vec<i64> = map_endpts
+        .iter()
+        .map(|&(_y, x)| x)
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect();
     input_src_endpts.sort();
 
     if input_src_endpts[0] > 0 {
         input_src_endpts.insert(0, 0);
         input_src_endpts.insert(1, input_src_endpts[1] - 1);
     }
-    if *input_src_endpts.last().unwrap() < std::i64::MAX {
+    if *input_src_endpts.last().unwrap() < i64::MAX {
         input_src_endpts.push(input_src_endpts.last().unwrap() + 1);
-        input_src_endpts.push(std::i64::MAX);
+        input_src_endpts.push(i64::MAX);
     }
 
-    let mut input_endpts: Vec<i64> = output_src_endpts.iter().chain(input_src_endpts.iter()).cloned().collect::<HashSet<_>>().into_iter().collect();
+    let mut input_endpts: Vec<i64> = output_src_endpts
+        .iter()
+        .chain(input_src_endpts.iter())
+        .cloned()
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .collect();
     input_endpts.sort();
 
     input_endpts
@@ -110,7 +150,7 @@ fn get_ans(mut seeds: Vec<i64>, m: &Vec<Vec<i64>>) -> Vec<i64> {
     for seed in &mut seeds {
         let mut new_seed = 0;
         for m in m {
-            if m[1] <= *seed && *seed <= m[1] + m[2] - 1 {
+            if m[1] <= *seed && *seed < m[1] + m[2] {
                 new_seed = m[0] - m[1] + *seed;
                 break;
             }
@@ -127,7 +167,7 @@ fn get_ans2(mut seeds: Vec<i64>, m: &Vec<(i64, i64, i64)>) -> Vec<i64> {
     for seed in &mut seeds {
         let mut new_seed = 0;
         for m in m {
-            if m.1 <= *seed && *seed <= m.1 + m.2 - 1 {
+            if m.1 <= *seed && *seed < m.1 + m.2 {
                 new_seed = m.0 - m.1 + *seed;
                 break;
             }
--- 2023/src/bin/09.rs
+++ 2023/src/bin/09.rs
@@ -3,12 +3,13 @@ advent_of_code::solution!(9);
 pub fn part_one(input: &str) -> Option<i64> {
     let a: i64 = input
         .lines()
-        .map(|line| line.split_whitespace()
-            .map(|s| s.parse::<i64>().unwrap())
-            .collect::<Vec<_>>())
-        .map(|v| solve(v))
-        .sum()
-        ;
+        .map(|line| {
+            line.split_whitespace()
+                .map(|s| s.parse::<i64>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .map(solve)
+        .sum();
 
     Some(a)
 }
@@ -16,13 +17,14 @@ pub fn part_one(input: &str) -> Option<i64> {
 pub fn part_two(input: &str) -> Option<i64> {
     let a: i64 = input
         .lines()
-        .map(|line| line.split_whitespace()
-            .map(|s| s.parse::<i64>().unwrap())
-            .rev()
-            .collect::<Vec<_>>())
-        .map(|v| solve(v))
-        .sum()
-        ;
+        .map(|line| {
+            line.split_whitespace()
+                .map(|s| s.parse::<i64>().unwrap())
+                .rev()
+                .collect::<Vec<_>>()
+        })
+        .map(solve)
+        .sum();
 
     Some(a)
 }
@@ -31,18 +33,16 @@ fn solve(a: Vec<i64>) -> i64 {
     let mut nums: Vec<Vec<i64>> = vec![a];
 
     while !nums.last().unwrap().iter().all(|&x| x == 0) {
-        nums.push(nums
-            .last()
-            .unwrap()
-            .windows(2)
-            .map(|w| w[1] - w[0])
-            .collect::<Vec<_>>())
+        nums.push(
+            nums.last()
+                .unwrap()
+                .windows(2)
+                .map(|w| w[1] - w[0])
+                .collect::<Vec<_>>(),
+        )
     }
 
-    nums
-        .iter()
-        .map(|v| v.last().unwrap())
-        .sum()
+    nums.iter().map(|v| v.last().unwrap()).sum()
 }
 
 #[cfg(test)]
